031fc385f446b57af75e92d02cb1159d298cca63
vivekdarkcoder <vivekprajapati@nimapinfotech.com>
2022-10-14 01:40:38 +0530

    change

--- server/routes/user.js
+++ server/routes/user.js
@@ -17,7 +17,13 @@ router.get(
     successRedirect: process.env.FRONTEND_URL,
   })
 );
-
+router.get("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+  });
 
 router.get("/me", isAuthenticated, myProfile)
 router.get("/logout", logout)
